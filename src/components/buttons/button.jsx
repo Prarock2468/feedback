ce7373199e13bf36e669072161c1ef3853fa1c45
Prarock2468 <prarock2468@gmail.com>
2025-05-13 01:01:09 +0530

    components added

---
+++ src/components/buttons/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({variyant , title , }) => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
---
+++ src/components/inputs/input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Input = (
+  { type, label, required }
+) => {
+  return (
+    <div>Input</div>
+  )
+}
+
+export default Input
